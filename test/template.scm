e14042b62db909823a242d0873a11a2866e72512
richo <richo@psych0tik.net>
2013-01-14 14:31:45 +1100

    Flesh out test coverage

--- test/template.scm
+++ test/template.scm
@@ -1,7 +1,15 @@
 (test-begin "template")
 
-(test "Should render templates without interpolated forms"
+(test "Should load templates without interpolated forms"
       "Rawr Test Thing\n"
       (load-template "test/no_interpolated_forms.html"))
+
+(test "Should render templates without interpolated forms"
+      "Rawr Test Thing\n"
+      (render-template "test/no_interpolated_forms.html"))
+
+(test "Should render templates without interpolated forms iwth locals"
+      "Rawr Test Thing\n"
+      (render-template "test/no_interpolated_forms.html" '((foo . "rawr"))))
 
 (test-end)
